03c07163630c5152eff944fbc1bd770c3b079312
Anuj Dutta <anuj@andhapp.com>
2012-03-24 00:48:17 +0000

    Added a message model; added haml gem and added debug statement to see if the UrlHelpers are being included or not and it seems thay are.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,5 @@
 class MessagesController < ApplicationController
+  def show
+    @message = Message.new
+  end
 end
--- spec/views/messages/show.html.haml_spec.rb
+++ spec/views/messages/show.html.haml_spec.rb
@@ -2,7 +2,11 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "messages/show.html.haml" do
   it "displays the text attribute of the message" do
+    @message = Message.new
+
     render
+
+    puts rendered
 
     rendered.should =~ /Hello world!!!/
     view.should render_template(:show)
